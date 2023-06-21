767541e5e4dda4880229e166903744397623866d
AmgadHasan <eng.amghasan@gmail.com>
2023-06-21 14:51:15 +0000

    Modified script to load user model and return recommended items

--- utils/user_rec_live.py
+++ utils/user_rec_live.py
@@ -2,16 +2,25 @@ import tensorflow as tf
 import pandas as pd
 
 class UserRecommender:
+    """A class that provides movie recommendations for a given user."""
     def __init__(self):
+        """Initializes the recommender by loading the pre-trained model and movie titles."""
         self.model = tf.keras.models.load_model('models/explicit_model')
-        self.movies = pd.read_csv('data/movies.csv')['title'].unique().reshape(-1,1)
+        self.movies = pd.read_csv('data/movies.csv')['title'].unique()
         
     def get_recommendations(self, user_id):
-        user_input = {"userId": tf.tile([["42"]], [9737, 1]), "movieTitle": self.movies}
-        recommended_items = self.model(user_input)
-        recommended_items = tf.gather(self.movies, tf.squeeze(tf.argsort(recommended_items, axis=0, direction='DESCENDING')))
-        return [item.decode('utf-8') for item in recommended_items[1].numpy()[0].tolist()]
-    
+        """Generate movie recommendations for a given user ID.
+        Args:
+          - user_id (str): The id of the user. It should be a string like "42"
+          
+        Returns: List of ranked movies recommendation e.g. ['Godfather', 'Godfather2']
+        """
+        model_input = {"userId": tf.tile(["42"], [9737]), "movieTitle": self.movies}
+        predicted_ratings = self.model(model_input)
+        recommended_items = tf.gather(self.movies, tf.squeeze(tf.argsort(predicted_ratings, axis=0, direction='DESCENDING')))
+        return recommended_items.numpy()
+        #return [item.decode('utf-8') for item in recommended_items[1].numpy()[0].tolist()]
+    # TO be deleted?
     def get_recommendations_from_csv(self, user_id):
         user_recommendations = self.recommendations_df.loc[user_id].tolist()
         return user_recommendations
